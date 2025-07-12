98bafdd840949cbe1b084a43bd80ed1dafa9a9e4
Mohamed <mosamag96@gmail.com>
2025-07-12 16:55:00 +0530

    locators

--- src/main/java/selenium/learning/java/selenium/basics/Locators.java
+++ src/main/java/selenium/learning/java/selenium/basics/Locators.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Locators {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
@@ -16,10 +16,20 @@ public class Locators {
 		driver.findElement(By.className("signInBtn")).click();
 		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 		driver.findElement(By.linkText("Forgot your password?")).click();
+		//Thread.sleep(2000);
 		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Shelby");
 		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("shelby@lts.com");
 		driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
 		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("shelby@ltd.com");
+		driver.findElement(By.xpath("//form/input[3]")).sendKeys("9878961");
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+		driver.findElement(By.cssSelector(".go-to-login-btn")).click();
+		driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul123");
+		driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.id("chkboxOne")).click();
+		driver.findElement(By.id("chkboxTwo")).click();
+		driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
 		
 		
 	}
